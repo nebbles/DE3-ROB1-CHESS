0193d3352309ae6a18933ada5fa00119ab9b1d6e
AnnaBernbaum <anna@bernbaum.co.uk>
2018-01-28 21:21:28 +0000

    vague layout of motion

---
+++ Layout.py
@@ -0,0 +1,19 @@
+# Convert game engine coordinates to real world x y coords class
+    # not our problem
+
+# Trejectory planning class
+    # up
+    # down
+    # across
+
+# Differentitiate trajectory wrt time to find velocity equation
+
+# Inverse kinematics to find joint angles
+    # update teh jacobian matrix
+    # update the current end effector velocity
+    # invert the jacobian
+    # angular velocity = inv jacobian * end effector velocity
+    # numerical integration to find joint angles
+    # Append angles, angular velocities, end effector
+
+# REdundancy resolution
