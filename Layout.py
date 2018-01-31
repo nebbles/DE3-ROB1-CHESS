b7b1be2df66a72891033237b5ff41ffe52b4eddf
AnnaBernbaum <anna@bernbaum.co.uk>
2018-01-31 12:47:29 +0000

    updating the base algorithm

--- Layout.py
+++ Layout.py
@@ -1,21 +1,31 @@
 # Convert game engine coordinates to real world x y coords class
     # not our problem
 
-# Trejectory planning class
+start = [x1; y1; z1];
+destination = [x2, y2, z2];
+
+# Trajectory of end effector planning class - outputs 3 trajectory equations (symbolic)
     # up
     # down
-    # across
+    # across inputs: pythagoras on given x and y coords
 
-# Differentitiate trajectory wrt time to find velocity equation
+# For each up, down, across:
+    # Differentitiate trajectory equations wrt time to find velocity equation (symbolic)
 
-# Inverse kinematics to find joint angles
-    # update teh jacobian matrix
-    # update the current end effector velocity
-    # invert the jacobian
-    # angular velocity = inv jacobian * end effector velocity
-    # numerical integration to find joint angles
-    # Append angles, angular velocities, end effector
+    # Inverse kinematics to find joint angles
+        # update teh jacobian matrix
+        # update the current end effector velocity
 
-# REdundancy resolution
-    # Moore Penrose Pseudo-Inverse minimises KE (OLS)
-    # pinv on MATLAB
+        # invert the jacobian
+        # angular velocity = inv jacobian * end effector velocity
+        # numerical integration to find joint angles
+        # Append angles, angular velocities, end effector
+
+    # join the up, down, across lists to create a total
+
+
+
+
+    # REdundancy resolution
+        # Moore Penrose Pseudo-Inverse minimises KE (OLS)
+        # pinv on MATLAB
