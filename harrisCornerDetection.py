ba3d0baccb6557481c074c28a6dadd8f4f06ab1b
leahpattison <leah.pattison@hotmail.co.uk>
2018-02-09 13:07:07 +0000

    Hough lines

--- harrisCornerDetection.py
+++ harrisCornerDetection.py
@@ -1,26 +1,24 @@
 import cv2
 import numpy as np
-import copy
 
-img = cv2.imread('emptyBoardCropped.jpg')
-img_orig = copy.copy(img)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+filename = 'emptyBoardCropped.jpg'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-# must give a float32 data type input
 gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.4)
+dst = cv2.cornerHarris(gray,2,3,0.03)
 
-# result is dilated for marking the corners, not important
-dst = cv2.dilate(dst, None)
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
 
 # Threshold for an optimal value, it may vary depending on the image.
-img[dst > 0.001 * dst.max()] = [0, 0, 255]
+img[dst>0.01*dst.max()]=[0,0,255]
 
-sift = cv2.xfeatures2d.SIFT_create(img)
+#sift = cv2.SIFT()
+#kp = sift.detect(img,None)
 
-cv2.imshow('Original', img_orig)
+img=cv2.drawKeypoints(img,kp,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-cv2.imshow('Original', sift)
-cv2.imshow('Harris Corner Detector', img)
+cv2.imshow('dst',img)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
--- sift.py
+++ sift.py
@@ -1,13 +1,30 @@
-from CVAnalyzer import CVAnalyzer
 import cv2
+import numpy as np
 
+filename = 'emptyBoardCropped.png'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-image = cv2.imread("emptyBoardCropped.jpg")
-board_image = cv2.imread("emptyBoardCropped.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# find Harris corners
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+dst = cv2.dilate(dst,None)
+ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+dst = np.uint8(dst)
 
-cv_analyzer = CVAnalyzer()
-harris = cv_analyzer.get_harris_corners(gray)
-sift = cv_analyzer.get_sift_descriptors(gray,harris)
-BIH	= cv_analyzer.find_board_image_homography (board_image)
+# find centroids
+ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
+# define the criteria to stop and refine the corners
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+# Now draw them
+res = np.hstack((centroids,corners))
+res = np.int0(res)
+img[res[:,1],res[:,0]]=[0,0,255]
+img[res[:,3],res[:,2]] = [0,255,0]
+
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
