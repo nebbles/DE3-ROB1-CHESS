7fce99a41b788ff616ac89ff8d3ee0435ce264b3
AnnaBernbaum <anna@bernbaum.co.uk>
2018-02-11 12:25:51 +0000

    deleting crap

--- venv/Lib/site-packages/sympy/physics/units/systems/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from sympy.physics.units.systems.mks import _mks_dim, MKS
-from sympy.physics.units.systems.mksa import _mksa_dim, MKSA
-from sympy.physics.units.systems.natural import _natural_dim, natural
