0161d4e91281fff691172c1d04864f3cb2558ade
AnnaBernbaum <anna@bernbaum.co.uk>
2018-02-18 13:20:53 +0000

    correcting plotting statemetns

--- motion/Trajectory.py
+++ motion/Trajectory.py
@@ -15,11 +15,9 @@ def intermediate_coords(rest, start, goal, hover):
 
 def create_line(a, b, v, vectors, dt):
     """function to create an array of poses between 2 points in space"""
-    #print('Start, End:', (a, b))
     vector = [b[0]-a[0], b[1]-a[1], b[2]-a[2]]  # vector from b to a
     vectors.append(vector)
     distance = sqrt(sum(i**2 for i in vector))  # straight line distance between points
-    #print('vector:', vector)
     # time to complete movement
     time = distance/v
 
@@ -51,7 +49,7 @@ def output(visual_flag=False):
     hover = 200  # hover height
 
     # selecting variables
-    dt = 0.01  # resolution in seconds 0.001
+    dt = 0.1  # resolution in seconds 0.001
     velocity = 5  # speed in mm/s
 
     # create complete array of poses
@@ -81,8 +79,6 @@ def output(visual_flag=False):
     l8 = np.delete(l8, 0, 0)
     l9 = np.delete(l9, 0, 0)
 
-    #print("BEN'S Vectors: ", vectors)
-
     # joining the lines into a trjectory
     line_list = (l1, l2, l3, l4, l5, l6, l7, l8, l9)
     trajectory = concatenate(line_list, axis=0)
@@ -102,15 +98,15 @@ def output(visual_flag=False):
     z_sample = [(trajectory[i][2]) for i in range(len(trajectory))]
 
     # interpolation to find smoothed trajectory
-    tck, u = interpolate.splprep([x_sample,y_sample,z_sample], s=1000000)  # s is amount of smoothness
-    x_knots, y_knots, z_knots = interpolate.splev(tck[0], tck)
+    tck, u = interpolate.splprep([x_sample,y_sample,z_sample], s=100000)  # s is amount of smoothness
+    x_knots, y_knots, z_knots = interpolate.splev(tck[0], tck) # finding knot locations
     u_fine = np.linspace(0, 1, len(trajectory))
     x_fine, y_fine, z_fine = interpolate.splev(u_fine, tck)
 
     # extracting the smooth trajectory
     smooth_trajectory = zip(x_fine, y_fine, z_fine)
     smooth_trajectory = [list(elem) for elem in smooth_trajectory]
-    smooth_trajectory = smooth_trajectory[::1000] # extracting every 1000th element
+    #smooth_trajectory = smooth_trajectory[::1000] # extracting every 1000th element when a smaller dt is used
 
     # Calculating vectors between points on smooth trajectory
     smooth_vectors=[]
@@ -123,6 +119,11 @@ def output(visual_flag=False):
         smooth_vector = [current[0] - previous[0], current[1] - previous[1], current[2] - previous[2]]
 
         smooth_vectors.append(smooth_vector)
+
+    # extracting x, y and z's from smooth_trajectory
+    x_smooth = [(smooth_trajectory[i][0]) for i in range(len(smooth_trajectory))]
+    y_smooth = [(smooth_trajectory[i][1]) for i in range(len(smooth_trajectory))]
+    z_smooth = [(smooth_trajectory[i][2]) for i in range(len(smooth_trajectory))]
 
     if visual_flag:
 
@@ -139,15 +140,17 @@ def output(visual_flag=False):
         # plotting
         fig = plt.figure()
         ax3d = fig.add_subplot(111, projection='3d')
-        ax3d.plot(x_sample, y_sample, z_sample, 'r*')  # plotting the angled points
+        ax3d.plot(x_sample, y_sample, z_sample, 'r')  # plotting the angled points
         ax3d.plot(x_knots, y_knots, z_knots, 'go')  # plotting the curve knots
-        ax3d.plot(x_fine, y_fine, z_fine, 'g') # plotting the smoothed trajectory
+        ax3d.plot(x_smooth, y_smooth, z_smooth, 'g') # plotting the smoothed trajectory
         plt.show()
+
 
     return smooth_vectors
 
 
 if __name__ == '__main__':
     output(visual_flag=True)
+    #output()
 
 
