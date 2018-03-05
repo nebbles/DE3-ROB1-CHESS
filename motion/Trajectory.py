d756c67e1b9deaa4b527d5abc71adb9b74da60c2
AnnaBernbaum <anna@bernbaum.co.uk>
2018-03-05 19:30:55 +0000

    Tidying

--- motion/Trajectory.py
+++ motion/Trajectory.py
@@ -11,11 +11,11 @@ def output(move, board_points, dead_zone, rest, hover, visual_flag=False):
     """ Calls the other functions, outputs list of coordinates to be followed by FRANKA
 
     Attributes:
-        * move: List of tuples outputted by the game engine
-        * board_points, dead_zone, rest, hover: Key locations collected from FRANKA
+        * ``move``: List of tuples outputted by the game engine
+        * ``board_points``, dead_zone, rest, hover: Key locations collected from FRANKA
 
     Returns:
-        * A list of x,y,z coordinated
+        * A list of ``x,y,z`` coordinated
     """
 
     # resolution - distance between each point in m
@@ -55,40 +55,23 @@ def output(move, board_points, dead_zone, rest, hover, visual_flag=False):
     return smooth_trajectory
 
 
-def best_fit(X, Y):
-
-    xbar = sum(X)/len(X)
-    ybar = sum(Y)/len(Y)
-    n = len(X) # or len(Y)
-
-    numer = sum([xi*yi for xi,yi in zip(X, Y)]) - n * xbar * ybar
-    denum = sum([xi**2 for xi in X]) - n * xbar**2
-
-    b = numer / denum
-    a = ybar - b * xbar
-
-    print('best fit line:\ny = {:.2f} + {:.2f}x'.format(a, b))
-
-    return a, b
-
-
 def logic(move):
     """Extracts information from move given by game engine
 
     Returns:
-        * A string ''None died" or "Died'' accordingly
+        * A string ``None died`` or ``Died`` accordingly
         * Algebraic notation (AN) of the start and goal
     """
 
-    if len(move)==1:
+    if len(move) == 1:
 
         # extract ANs
         start_AN = (move[0][1])[:2]
         goal_AN = (move[0][1])[2:4]
 
-        return "None died", start_AN, goal_AN # no died_AN
+        return "None died", start_AN, goal_AN
 
-    elif len(move)==2:
+    elif len(move) == 2:
 
         # extract ANs
         start_AN = (move[1][1])[:2]
@@ -98,11 +81,11 @@ def logic(move):
 
 
 def AN_to_coords(AN, board_points):
-    """Converts algebraic notation into real world x,y,z, coordinates
+    """Converts algebraic notation into real world ``x, y, z`` coordinates
 
     Attributes:
-        * "AN": The algebraic notation from the game engine
-        * "board_points": The collected coordinates on the FRANKA frame of the corners of the board
+        * ``AN``: The algebraic notation from the game engine
+        * ``board_points``: The collected coordinates on the FRANKA frame of the corners of the board
 
     """
 
@@ -110,10 +93,8 @@ def AN_to_coords(AN, board_points):
     A8 = board_points[1]
     H8 = board_points[2]
 
-    x = H8[0] - A1[0] # square width in FRANKA units
-    y = A8[1] - A1[1] # square length in FRANKA units
-
-    n = 8  # number of squares
+    x = H8[0] - A1[0]  # square width in FRANKA units
+    y = A8[1] - A1[1]  # square length in FRANKA units
 
     # find coordinates of each AN
     letters = dict([('a', x / 2), ('b', (x / 2) * 2), ('c', (x / 2) * 3), ('d', (x / 2) * 4), ('e', (x / 2) * 5),
@@ -165,17 +146,17 @@ def intermediate_coords(rest, start, goal, hover, dead_zone):
     """Generate the intermediate positions of FRANKA along the path
 
     The attributes are the 5 known locations of FRANKA:
-        * "rest"
-        * "start"
-        * "goal"
-        * "hover"
-        * "dead_zone"
+        * ``rest``
+        * ``start``
+        * ``goal``
+        * ``hover``
+        * ``dead_zone``
 
     Returns the desired intermediate positions:
-        * "rest_h", rest hover
-        * "start_h", start hover
-        * "goal_h", goal hover
-        * "deadzone_h", deadzone hover
+        * ``rest_h``, rest hover
+        * ``start_h``, start hover
+        * ``goal_h``, goal hover
+        * ``deadzone_h``, deadzone hover
     """
 
     rest_h = [rest[0], rest[1], hover]
@@ -187,7 +168,7 @@ def intermediate_coords(rest, start, goal, hover, dead_zone):
 
 
 def create_line(a, b, n):
-    """Generates a list of coordinates "n" metres apart between 2 points "a" and "b" in space."""
+    """Generates a list of coordinates ``n`` metres apart between 2 points ``a`` and ``b`` in space."""
 
     vector = [b[0] - a[0], b[1] - a[1], b[2] - a[2]]  # vector from b to a
     distance = sqrt(sum(i ** 2 for i in vector))
@@ -213,7 +194,7 @@ def create_line(a, b, n):
 
 
 def start_path(dead_status, rest, start_h, goal_h, n):
-    """ Generate the path from the rest position to the first location, based on the "dead_status" """
+    """ Generate the path from the rest position to the first location, based on the ``dead_status`` """
 
     if dead_status == 'None died':
         l0 = create_line(rest, start_h, n)
@@ -271,12 +252,12 @@ def generate_trajectory(line_list, dead_status):
     """Joins together the separate lines to make a full trajectory
 
     Attributes:
-        *"line_list": A list of lines, each of which is made up of many x,y,z coordinates
-        *"dead_status": Determines what trajectory to generate, based on whether a piece has died
+        *``line_list``: A list of lines, each of which is made up of many x,y,z coordinates
+        *``dead_status``: Determines what trajectory to generate, based on whether a piece has died
 
     Returns:
-        *"trajectory": A list of coordinates starting and ending at the rest position
-        *"trajectory_n": A trajectory of points between either a rest position and a gripping action, or 2 gripping actions
+        *``trajectory``: A list of coordinates starting and ending at the rest position
+        *``trajectory_n``: A trajectory of points between either a rest position and a gripping action, or 2 gripping actions
     """
 
     trajectory = concatenate(line_list, axis=0)
@@ -314,7 +295,7 @@ def generate_trajectory(line_list, dead_status):
 
 
 def data_split(trajectory):
-    """Splits the data into x, y,z"""
+    """Splits the data into ``x, y, z``"""
 
     x = [(trajectory[i][0]) for i in range(len(trajectory))]
     y = [(trajectory[i][1]) for i in range(len(trajectory))]
@@ -336,10 +317,10 @@ def smooth(trajectory):
     """ Generates a smooth version of the inputted trajectory
 
     Returns:
-        * "smooth_trajetory": The smoothed trajectory as a list of coordinates
-        * "x_sample, y_sample, z_sample" : x, y, z coordinates of the inputted trajectory
-        * "x_knots, y_knots, z_knots": x, y, z coordinates of the knot points
-        * "x_smooth, y_smooth, z_smooth": x, y, z coordinates of the smoothed trajectory
+        * ``smooth_trajectory``: The smoothed trajectory as a list of coordinates
+        * ``x_sample, y_sample, z_sample`` : ``x, y, z`` coordinates of the inputted trajectory
+        * ``x_knots, y_knots, z_knots``: ``x, y, z`` coordinates of the knot points
+        * ``x_smooth, y_smooth, z_smooth``: ``x, y, z`` coordinates of the smoothed trajectory
     """
 
     x_sample, y_sample, z_sample = data_split(trajectory)
@@ -367,7 +348,7 @@ def generate_vectors(trajectory):
 
 
 def plot(x_sample, y_sample, z_sample, x_knots, y_knots, z_knots, x_smooth, y_smooth, z_smooth):
-    """Plot the initial trajectory and it's smoothed version"""
+    """Plot the initial trajectory and its smoothed version"""
 
     fig = plt.figure()
     ax3d = fig.add_subplot(111, projection='3d')
