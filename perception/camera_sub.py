b47580c3b208098d6ca29e83e9b7f6a34dd940a4
SylviaZhanggg <35939392+SylviaZhanggg@users.noreply.github.com>
2018-03-06 16:59:19 +0000

    Update camera_sub.py

--- perception/camera_sub.py
+++ perception/camera_sub.py
@@ -18,43 +18,35 @@ import cv2
 
 #dont forget to roslaunch openni2_launch openni2.launch
 #ros core
- 
-#test_image = None
-#est_depth = None
                                                                                                                      
 class image_converter:
 
   def __init__(self):
-
-    global cv_image, depth_image
-    #self.image_pub = rospy.Publisher("/camera/rgb/image_rect_color",Image)
+    
+    self.cv_image = 0
+    self.depth_image = 0
+  
 
     self.bridge = CvBridge()
-    # self.image = None
-    # self.depth = None
-
     image_sub = Subscriber("/camera/rgb/image_rect_color", Image)
     depth_sub = Subscriber("/camera/depth_registered/image_raw", Image)
 
     #self.image_pub = rospy.Publisher("/image", Image, queue_size = 10)
     #self.depth_pub = rospy.Publisher("/depth", Image, queue_size = 10)
     
-    tss = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub],queue_size=10, slop=0.5)
-    #self.callback_d(image_sub,depth_sub)                                    
+    tss = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub],queue_size=10, slop=0.5)                                   
     tss.registerCallback(self.callback)
-    # print(self.depth)
     
     print('init')
 
   def callback(self, img, depth, debug=False):
 
     self.flag = 1
-
+    #global cv_image, depth_image
     try:
       cv_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
     except CvBridgeError as e:
       print(e)
-    # self.image_pub.publish(cv_image)
 
 
     try:
@@ -62,12 +54,12 @@ class image_converter:
       depth_image = ((255 * depth_image_raw)).astype(np.uint8)
     except CvBridgeError as e:
       print(e)
-    #self.depth_pub.publish(depth_image)
 
     #else:
         # Save OpenCV2 image as a jpeg 
         #cv2.imwrite('camera_image.jpeg', cv_image)
-	#cv2.imwrite('depth_image.jpeg', depth_image)
+        #cv2.imwrite('depth_image.jpeg', depth_image)
+    
     if debug:
       cv2.waitKey(1)
       cv2.imshow("Image window", cv_image)
@@ -81,30 +73,29 @@ class image_converter:
 
 
 def main(args): 
-  ic = image_converter()
 
+  #global cv_image, depth_image
+
+  ic = image_converter()
   rospy.init_node('image_converter', anonymous=True)
-  
+   
   try:
 
+    #rospy.spin()
+    
     while True:
       #wait for signal
-
       #sees a thing
-
       #gets frame from ic. They are both global variables so whenever the callback function is called
       #these variables update.
-      currentRGB = cv_image
-      currentDepth = depth_image
+      currentRGB = ic.cv_image
+      currentDepth = ic.depth_image
 
 
   except KeyboardInterrupt:
     print("Shutting down")
   cv2.destroyAllWindows()
-
-#def callback(self, data) :
     
-
 
 if __name__ == '__main__':
     main(sys.argv)
