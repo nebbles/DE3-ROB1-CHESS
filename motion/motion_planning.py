410ff20b369fc61a96886cf9f0a956b93ef2c6da
AnnaBernbaum <anna@bernbaum.co.uk>
2018-03-09 16:57:23 +0000

    Correcting AN xyz conversion

--- motion/motion_planning.py
+++ motion/motion_planning.py
@@ -1,19 +1,23 @@
-#from franka.franka_control_ros import FrankaRos
+from __future__ import print_function
+#from builtins import input
 from numpy import mean
 from matplotlib.pyplot import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # for: fig.gca(projection = '3d')
 import decimal
 
-# TODO: Add pyhton 2 compatibility
+# Python 2/3 raw input correction
+try:
+   input = raw_input
+except NameError:
+   pass
+
 # TODO: install ros packages to run with Franka
 # TODO: Sort out AN to real life conversion
 
 class MotionPlanning:
     def __init__(self):
         """Gets the location of each corner of the board and stores them"""
-
-        #arm = FrankaRos()  # debug=True)
 
         print("For the following commands, place the FRANKA end effector in the centre of the relevant square\n")
 
@@ -51,22 +55,65 @@ class MotionPlanning:
             H1 = [0.468, -0.138, -0.0156]
 
         self.board_points = [A1, A8, H8, H1]
+        self.A1 = A1
+        self.A8 = A8
+        self.H8 = H8
+        self.H1 = H1
+
+        # Find the maximum z value of the board
+        max_z = max(A1[2], A8[2], H1[2], H8[2])
+
+        # Find x axis vector
+        x_vector_1 = [(A8[0] - H8[0]), (A8[1] - H8[1]), max_z]
+        x_vector_2 = [(A1[0] - H1[0]), (A1[1] - H1[1]), max_z]
+        x_vector = [sum(x)/2 for x in zip(x_vector_1, x_vector_2)]
+        self.x_unit_vector = [coord/8 for coord in x_vector]
+
+        # Find y axis vector
+        y_vector_1 = [(H1[0] - H8[0]), (H1[1] - H8[1]), max_z]
+        y_vector_2 = [(A1[0] - A8[0]), (A1[1] - A8[1]), max_z]
+        y_vector = [sum(x)/2 for x in zip(y_vector_1, y_vector_2)]
+        self.y_unit_vector = [coord/8 for coord in y_vector]
 
         # Find hover height
         board_points_z = [corner[2] for corner in self.board_points]
         self.hover_height = mean(board_points_z) + 0.2  # hard coded hover height
 
         # Find location of deadzone
-        deadzone_x = mean([A1[0], A8[0]]) + 0.15 # hard coded x coordinate of deadzone
-        deadzone_y = (A8[1] - A1[1])/2 # exact centre of board
-        deadzone_z = mean(board_points_z) + 0.1 # hardcoded z coordinate of deadzone
-        self.deadzone = [deadzone_x, deadzone_y, deadzone_z]
+        deadzone_x_vector = [-(i * 4) for i in self.x_unit_vector]
+        deadzone_y_vector = [i * 4 for i in self.y_unit_vector]
+        deadzone_z = max_z + 0.1 # hardcoded z coordinate of deadzone
+        deadzone = [sum(i) for i in zip(self.H8, deadzone_x_vector, deadzone_y_vector)]
+        deadzone[2] = deadzone_z
+        self.deadzone = deadzone
 
         # Find the location of the rest position
-        rest_x = (H1[0] - A1[0])/2
-        rest_y = (A8[1] - A1[1])/2
-        rest_z = mean(board_points_z) + 0.9
-        self.rest = [rest_x, rest_y, rest_z]
+        rest_x_vector = [i * 4 for i in self.x_unit_vector]
+        rest_y_vector = [i * 3 for i in self.y_unit_vector]
+        rest_z = max_z + 0.9  # hardcoded z coordinate of the rest position
+        rest = [sum(i) for i in zip(self.H8, rest_x_vector, rest_y_vector)]
+        rest[2] = rest_z
+        self.rest = rest
+
+        self.letters = dict([('a', [i * 0.5 for i in self.x_unit_vector]),
+                        ('b', [i * 1.5 for i in self.x_unit_vector]),
+                        ('c', [i * 2.5 for i in self.x_unit_vector]),
+                        ('d', [i * 3.5 for i in self.x_unit_vector]),
+                        ('e', [i * 4.5 for i in self.x_unit_vector]),
+                        ('f', [i * 5.5 for i in self.x_unit_vector]),
+                        ('g', [i * 6.5 for i in self.x_unit_vector]),
+                        ('h', [i * 7.5 for i in self.x_unit_vector])
+                        ])
+
+        self.numbers = dict([('1', [i * 0.5 for i in self.y_unit_vector]),
+                        ('2', [i * 1.5 for i in self.y_unit_vector]),
+                        ('3', [i * 2.5 for i in self.y_unit_vector]),
+                        ('4', [i * 3.5 for i in self.y_unit_vector]),
+                        ('5', [i * 4.5 for i in self.y_unit_vector]),
+                        ('6', [i * 5.5 for i in self.y_unit_vector]),
+                        ('7', [i * 6.5 for i in self.y_unit_vector]),
+                        ('8', [i * 7.5 for i in self.y_unit_vector])
+                        ])
 
     def make_move(self, move, visual_flag=False):
 
@@ -98,21 +145,22 @@ class MotionPlanning:
         elif dead_status == 'Died':
             path = [self.rest, goal_h, goal, goal_h, deadzone_h, self.deadzone, deadzone_h, start_h, start, start_h, goal_h, goal, goal_h, self.rest]
 
-        x_raw = [coord[0] for coord in path]
-        y_raw = [coord[1] for coord in path]
-        z_raw = [coord[2] for coord in path]
-
-        # Removing excess precision
-        x = [decimal.Decimal(i) for i in x_raw]
-        x = [float(round(i, 3)) for i in x]
-
-        y = [decimal.Decimal(i) for i in y_raw]
-        y = [float(round(i, 3)) for i in y]
-
-        z = [decimal.Decimal(i) for i in z_raw]
-        z = [float(round(i, 3)) for i in z]
+        # # Removing excess precision
+        # x = [decimal.Decimal(i) for i in x_raw]
+        # x = [float(round(i, 3)) for i in x]
+        #
+        # y = [decimal.Decimal(i) for i in y_raw]
+        # y = [float(round(i, 3)) for i in y]
+        #
+        # z = [decimal.Decimal(i) for i in z_raw]
+        # z = [float(round(i, 3)) for i in z]
 
         if visual_flag:
+
+            x = [coord[0] for coord in path]
+            y = [coord[1] for coord in path]
+            z = [coord[2] for coord in path]
+
             fig = plt.figure()
             ax3d = fig.add_subplot(111, projection='3d')
 
@@ -126,51 +174,23 @@ class MotionPlanning:
 
             ax3d.plot(board_x, board_y, board_z, 'b')  # plot the board
             ax3d.plot(x, y, z, 'r')  # plot path
+            ax3d.plot([self.rest[0]], [self.rest[1]], [self.rest[2]], 'g*')
+            ax3d.plot([self.deadzone[0]], [self.deadzone[1]], [self.deadzone[2]], 'g*')
+
             plt.show()
 
         print(path)
         return path
 
     def AN_to_coords(self, AN):
-        """Converts algebraic notation into real world ``x, y, z`` coordinates
-
-        Attributes:
-            * ``AN``: The algebraic notation from the game engine
-            * ``board_points``: The collected coordinates on the FRANKA frame of the corners of the board
-
-        """
-
-        # Location of corners of the board
-        A1 = self.board_points[0]
-        A8 = self.board_points[1]
-        H8 = self.board_points[2]
-        H1 = self.board_points[3]
-
-
-        x1 = abs((H1[0] - A1[0]) / 8)
-        x2 = abs((H8[0] - A8[0]) / 8)
-        x = mean([x1, x2])  # square width in FRANKA units
-        y1 = abs((A8[1] - A1[1]) / 8)
-        y2 = abs((H8[1] - H1[1]) / 8)
-        y = mean([y1, y2])  # square length in FRANKA units
-
-        # find coordinates of each AN TODO CORRECT THIS!!!
-
-        # is x axis in the given x, y z coords, the same as the x axis of the board?? CRITICAL
-        letters = dict([('a', A1[0] + 0.5 * x), ('b', A1[0] + 1.5 * x), ('c', A1[0] + 2.5 * x), ('d', A1[0] + 3.5 * x),
-                        ('e', A1[0] + 4.5 * x),
-                        ('f', A1[0] + 5.5 * x), ('g', A1[0] + 6.5 * x), ('h', A1[0] + 7.5 * x)])
-        numbers = dict([('1', A1[1] + 0.5 * y), ('2', A1[1] + 1.5 * y), ('3', A1[1] + 2.5 * y), ('4', A1[1] + 3.5 * y),
-                        ('5', A1[1] + 4.5 * y),
-                        ('6', A1[1] + 5.5 * y), ('7', A1[1] + 6.5 * y), ('8', A1[1] + 7.5 * y)])
+        """Converts algebraic notation into real world ``x, y, z`` coordinates"""
 
         # selecting the location
         letter = AN[0]
         number = AN[1]
-        x = numbers[number]
-        y = letters[letter]
-        z = 0
-
-        coord = [x, y, z]
+        x = self.numbers[number]
+        y = self.letters[letter]
+        coord = [sum(i) for i in zip(self.H8, x, y)]
+        # set z?
 
         return coord
--- test_motion_3.py
+++ test_motion_3.py
@@ -1,7 +1,11 @@
 from motion import motion_planning
+#from franka.franka_control_ros import FrankaRos
+
+#arm = FrankaRos()  # debug=True)
 
 # Instantiate the motion class
 FRANKA = motion_planning.MotionPlanning()
 
 #FRANKA.make_move([("r", "a1a6")], visual_flag=True)
+#arm = FrankaRos()  # debug=True)
 FRANKA.make_move([("r", "b4"), ("r", "a1b4")], visual_flag=True)
