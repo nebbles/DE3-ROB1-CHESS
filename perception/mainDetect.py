920b4b04278de1494b3986dcd82cbe7de0095bb2
Paolo Rüegg <paolo.rueegg@me.com>
2018-03-09 15:18:12 +0000

    mask change

--- perception/mainDetect.py
+++ perception/mainDetect.py
@@ -222,16 +222,16 @@ class Perception:
             cv2.imwrite("FilteredContours.jpeg", imgContours)
 
         # Create new all black image
-        mask = np.zeros((img.shape[0], img.shape[1]), 'uint8')
+        mask = np.zeros((img.shape[0], img.shape[1]), 'uint8')*125
         # Copy the chessboard edges as a filled white polygon
         cv2.fillConvexPoly(mask, chessboardEdge, 255, 1)
         # Assign all pixels to out that are white (i.e the polygon, i.e. the chessboard)
-        extracted = np.zeros_like(img)
+        extracted = np.zeros_like(img)*125
         extracted[mask == 255] = img[mask == 255]
         # Make mask green in order to facilitate removal of the red strip around chessboard
-        extracted[np.where((extracted == [0, 0, 0]).all(axis=2))] = [0, 0, 100]
+        extracted[np.where((extracted == [125, 125, 125]).all(axis=2))] = [0, 0, 20]
         # Adds same coloured line to remove red strip based on chessboard edge
-        cv2.polylines(extracted, [chessboardEdge], True, (0, 0, 100), thickness=15)
+        cv2.polylines(extracted, [chessboardEdge], True, (0, 0, 20), thickness=15)
 
         if debug:
             cv2.imshow("Masked", extracted)
