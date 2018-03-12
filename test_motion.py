b29fc767492bd44bc19750e349cc1f32a0b6b354
nebbles <bsg115@ic.ac.uk>
2018-03-12 02:40:23 +0000

    minor updates to test_motion

--- test_motion.py
+++ test_motion.py
@@ -3,9 +3,7 @@ from motion import motion_planning
 
 #arm = FrankaRos()  # debug=True)
 
-# Instantiate the motion class
-FRANKA = motion_planning.MotionPlanning()
+planner = motion_planning.MotionPlanning()
 
-#FRANKA.make_move([("r", "a1a6")], visual_flag=True)
-#arm = FrankaRos()  # debug=True)
-FRANKA.make_move([("r", "b4"), ("r", "a1b4")], visual_flag=True)
+# planner.make_move([("r", "a1a6")], visual_flag=True)
+# planner.make_move([("r", "b4"), ("r", "a1b4")], visual_flag=True)
