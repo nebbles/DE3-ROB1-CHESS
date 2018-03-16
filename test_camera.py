ce3c370b648490d9146f606d4e6e73321a8a8fd3
nebbles <bsg115@ic.ac.uk>
2018-03-16 18:02:45 +0000

    got it workng with cam and franka control simultaneously

--- test_camera.py
+++ test_camera.py
@@ -12,26 +12,38 @@ import rospy
 import time
 import camera_subscriber
 from franka.franka_control_ros import FrankaRos
+from tortoise_hare2_anna import MotionPlanner
+import numpy as np
 
 
 def main():
     rospy.init_node('test_camera_node', anonymous=True)
     feed = camera_subscriber.CameraFeed()
-    # arm = FrankaRos()
+    arm = FrankaRos()
 
+    planner = MotionPlanner(arm, visual=False, manual_calibration=False, debug=True)
+  
     # Main loop - temporary
     while True:
         start_clk = time.time()
         while time.time() < start_clk + 5:
             time.sleep(0.05)  # refresh rate of camera frames
 
-            # print("X pos: ", arm.x)
+            print("X pos: ", arm.x)
 
             rgb_frame, depth_frame = feed.get_frames()
 
             cv2.imshow("FETCHED RGB", rgb_frame)
             cv2.imshow("FETCHED Depth", depth_frame)
             cv2.waitKey(1)
+
+        time.sleep(1)
+        path_5 = np.array([[arm.x, arm.y, arm.z], [arm.x+0.2, arm.y+0.3, arm.z]])  # move +y
+        motion_plan = planner.apply_trapezoid_vel_profile(path_5)
+
+        for x, y, z, speed in motion_plan:
+            arm.move_to(x, y, z, speed)
+            time.sleep(0.005)  # control loop
 
         # feed.stop_subscribers()
         # time.sleep(5)
