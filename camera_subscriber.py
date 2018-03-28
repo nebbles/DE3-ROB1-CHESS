154e7820035bd91949d4f4030bc3ad032bf0a399
nebbles <bsg115@ic.ac.uk>
2018-03-28 16:09:39 +0100

    remove start/stop subscribers for camera_subscriber.py

--- camera_subscriber.py
+++ camera_subscriber.py
@@ -31,9 +31,11 @@ class CameraFeed:
         if init_ros_node:
             rospy.init_node('image_converter', anonymous=True)
 
-        self.start_subscribers()
-
-        
+        self.image_sub = Subscriber("/camera/rgb/image_rect_color", Image)
+        self.depth_sub = Subscriber("/camera/depth_registered/image_raw", Image)
+        self.tss = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.depth_sub],
+                                                               queue_size=10, slop=0.5)
+        self.tss.registerCallback(self.callback)
 
         time.sleep(0.5)
 
@@ -44,37 +46,29 @@ class CameraFeed:
         if self.debug:
             print('Camera feed initialisation successful.')
 
-    def callback(self, img, depth, debug=False):
+    def callback(self, img, depth):
         self.rgb_raw = img
         self.depth_raw = depth
         if self.debug:
             print("New images have been collected.")
 
     def get_frames(self):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(self.rgb_raw, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+        cv_image = None
+        depth_image = None
+        while cv_image is None and depth_image is None:
+            try:
+                cv_image = self.bridge.imgmsg_to_cv2(self.rgb_raw, "bgr8")
+            except CvBridgeError as e:
+                print(e)
 
-        try:
-            depth_image_raw = self.bridge.imgmsg_to_cv2(self.depth_raw, "passthrough")
-            depth_image = ((255 * depth_image_raw)).astype(np.uint8)
-        except CvBridgeError as e:
-            print(e)
+            try:
+                depth_image_raw = self.bridge.imgmsg_to_cv2(self.depth_raw, "passthrough")
+                # noinspection PyRedundantParentheses
+                depth_image = ((255 * depth_image_raw)).astype(np.uint8)
+            except CvBridgeError as e:
+                print(e)
 
         if self.debug:
             print("Image has been converted.")
 
         return cv_image, depth_image
-
-    def stop_subscribers(self):
-        self.image_sub.unregister()
-        self.depth_sub.unregister()
-        self.tss = None
-
-    def start_subscribers(self):
-        self.image_sub = Subscriber("/camera/rgb/image_rect_color", Image)
-        self.depth_sub = Subscriber("/camera/depth_registered/image_raw", Image)
-        self.tss = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.depth_sub], queue_size=10, slop=0.5)
-        self.tss.registerCallback(self.callback)
-
